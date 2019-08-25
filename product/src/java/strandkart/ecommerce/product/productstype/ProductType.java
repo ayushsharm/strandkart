7811f0ca1660f7a01ac8483101adfd32744b76fd
Ayush Sharma <ayush.sharma@strandls.com>
2019-08-26 02:37:16 +0530

    Added getter method

--- product/src/java/strandkart/ecommerce/product/productstype/ProductType.java
+++ product/src/java/strandkart/ecommerce/product/productstype/ProductType.java
@@ -3,13 +3,13 @@ package strandkart.ecommerce.product.productstype;
 import java.util.ArrayList;
 import java.util.List;
 
-public enum Products {
+public enum ProductType {
     BOOKS("Books"),
     ;
 
     String displayname;
 
-    Products(String displayname) {
+    ProductType(String displayname) {
         this.displayname = displayname;
     }
 
@@ -17,8 +17,8 @@ public enum Products {
         return displayname;
     }
 
-    public static List<Products> getAllProducts() {
-        List<Products> allProducts = new ArrayList<Products>();
+    public static List<ProductType> getAllProducts() {
+        List<ProductType> allProducts = new ArrayList<ProductType>();
         allProducts.add(BOOKS);
 
         return allProducts;
