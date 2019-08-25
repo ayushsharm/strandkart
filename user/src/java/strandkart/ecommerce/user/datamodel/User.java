491cda2e28750ee73461f38162f652afe57b3673
Ayush Sharma <ayush.sharma@strandls.com>
2019-08-26 02:38:16 +0530

    New Datamodel User added

--- user/src/java/strandkart/ecommerce/user/datamodel/User.java
+++ user/src/java/strandkart/ecommerce/user/datamodel/User.java
@@ -1,4 +1,21 @@
 package strandkart.ecommerce.user.datamodel;
 
 public class User {
+
+    private String name;
+
+    private String contactNumber;
+
+    public User(String name, String contactNumber) {
+        this.contactNumber = contactNumber;
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return name;
+    }
+
+    public String getUserContactNumber() {
+        return contactNumber;
+    }
 }
