c6a04e852fa33ebd34c3f07d3ad4890dbc7215f9
Ayush Sharma <ayush.sharma@strandls.com>
2019-08-26 02:36:43 +0530

    aa

--- product/src/java/strandkart/ecommerce/product/DataModel/Product.java
+++ product/src/java/strandkart/ecommerce/product/DataModel/Product.java
@@ -1,4 +1,15 @@
 package strandkart.ecommerce.product.DataModel;
 
+import strandkart.ecommerce.product.productstype.ProductType;
+
 public class Product {
+    private ProductType type;
+
+    public Product(ProductType type) {
+        this.type = type;
+    }
+
+    public ProductType getProductType() {
+        return type;
+    }
 }
