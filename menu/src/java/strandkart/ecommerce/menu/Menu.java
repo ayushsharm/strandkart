2e500d424eb50c1e544f815cb0a3fbe70ad34888
Ayush Sharma <ayush@panhala.strandls.com>
2019-08-26 19:30:54 +0530

    Modifications done to have only menu display function in this file

--- menu/src/java/strandkart/ecommerce/menu/Menu.java
+++ menu/src/java/strandkart/ecommerce/menu/Menu.java
@@ -1,98 +1,44 @@
 package strandkart.ecommerce.menu;
 
+import com.google.inject.Inject;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import strandkart.ecommerce.book.Binding;
-import strandkart.ecommerce.book.Datamodel.Books;
+import strandkart.ecommerce.book.Datamodel.Book;
+import strandkart.ecommerce.book.service.StrandKartBookDetails;
 import strandkart.ecommerce.orders.DataModels.Order;
 import strandkart.ecommerce.product.productstype.ProductType;
 import strandkart.ecommerce.user.datamodel.User;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class Menu {
-
-    public static Set<HashMap<String, Object>> findMatchingBooksWithTitle(ArrayList<HashMap<String, Object>> bookMapList, String bookName) {
-        Set<HashMap<String, Object>> booksFound = new HashSet<HashMap<String, Object>>();
-        for (HashMap<String, Object> map : bookMapList) {
-            if (bookName.equals(map.get("Title").toString())) {
-                booksFound.add(map);
-            }
-        }
-        return booksFound;
-    }
-
-    public static HashMap<String, Object> findMatchingBookWithISBN(Set<HashMap<String, Object>> set, String isbn) {
-        HashMap<String, Object> bookFound = new HashMap<String, Object>();
-        for(HashMap<String, Object> map1: set){
-            if(map1.get("ISBN").toString().equals(isbn)){
-                return bookFound;
-            }
-        }
-        return null;
-    }
+    @Inject
+    static
+    StrandKartBookDetails strandKartBookDetails;
 
 
     public static void main(String[] args) throws IOException {
 
-        JSONParser jsonParser = new JSONParser();
-        List<JSONObject> bookJSONList = new ArrayList<JSONObject>();
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("books.txt"));
-            String line = reader.readLine();
-            while (line != null) {
-                Object obj = jsonParser.parse(line);
-                JSONObject jsonObject = (JSONObject) obj;
-                bookJSONList.add(jsonObject);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        ArrayList<HashMap<String, Object>> bookMapList = new ArrayList<HashMap<String, Object>>();
-        HashMap<String, Object> bookMap = new HashMap<String, Object>();
-        List<Books> allBooks = new ArrayList<Books>();
-
-        List<Order> orderList = new ArrayList<Order>();
-
-        for (JSONObject jsonObject : bookJSONList) {
-            bookMap.clear();
-            bookMap.put("Title", jsonObject.get("Title").toString());
-            bookMap.put("Author", jsonObject.get("Author").toString());
-            bookMap.put("ISBN", jsonObject.get("ISBN").toString());
-            bookMap.put("Publisher", jsonObject.get("Publisher").toString());
-            bookMap.put("Language", jsonObject.get("Language").toString());
-            bookMap.put("Year", jsonObject.get("Year").toString());
-            bookMap.put("Binding", jsonObject.get("Binding").toString());
-            bookMap.put("Price", jsonObject.get("Price").toString());
-            bookMapList.add(bookMap);
-        }
-
-
-        System.out.println("WELCOME TO STRANDKART.\n" +
+        System.out.println("WELCOME TO STRAND-KART.\n" +
                 "Select Option to Continue\n" +
                 "1. List all Products.\n" +
-                "2. List all Books.\n" +
+                "2. List all Book.\n" +
                 "3. Search a particular book.\n" +
                 "4. Store a new book.\n" +
                 "5. Purchase a book.\n" +
                 "6. Exit.");
 
+        Scanner input = new Scanner(System.in);
+
         int choice = 0;
         while (choice != 6) {
-            Scanner input = new Scanner(System.in);
+
             choice = input.nextInt();
+
             switch (choice) {
+
                 case 1:
                     List<ProductType> products = ProductType.getAllProducts();
                     System.out.println("List of Products : ");
@@ -101,16 +47,16 @@ public class Menu {
                     }
                     break;
                 case 2:
-                    if (bookMapList.size() > 0) {
-                        System.out.println(bookMapList);
-                    } else System.out.println("No books in stock.");
+                    List<Book> allBooks = strandKartBookDetails.getAllBooks();
+                    for(Book book : allBooks){
+                        System.out.println(book.toString());
+                    }
                     break;
                 case 3:
                     input.nextLine();
                     System.out.println("Please enter the name of the book");
                     String bookName = input.nextLine();
-                    System.out.println(bookName);
-                    Set<HashMap<String, Object>> booksFound = findMatchingBooksWithTitle(bookMapList, bookName);
+                    strandKartBookDetails
                     if(booksFound.size()==0){
                         System.out.println("No book with this title is available on StrandKart.");
                     }
@@ -152,7 +98,7 @@ public class Menu {
                     }
                     System.out.print("Price : ");
                     Float price = input.nextFloat();
-                    Books book = new Books(ProductType.BOOKS, title, author, isbn, publisher, language, year, binding, price);
+                    Book book = new Book(ProductType.BOOKS, title, author, isbn, publisher, language, year, binding, price);
                     allBooks.add(book);
                     bookMap.put("Title", title);
                     bookMap.put("Author", author);
@@ -199,7 +145,7 @@ public class Menu {
                         else if(bookfound.get("Binding").toString().equals("DIGITAL")){
                             binding1 = Binding.DIGITAL;
                         }
-                        Books bookToPurchase = new Books(ProductType.BOOKS, bookfound.get("Title").toString(),
+                        Book bookToPurchase = new Book(ProductType.BOOKS, bookfound.get("Title").toString(),
                                 bookfound.get("Author").toString(),bookfound.get("ISBN").toString(),
                                 bookfound.get("Publisher").toString(), bookfound.get("Language").toString(),
                                 bookfound.get("Year").toString(), binding1, (Float)bookfound.get("Price"));
@@ -226,7 +172,7 @@ public class Menu {
             if (choice != 6) {
                 System.out.println("Select Option to Continue\n" +
                         "1. List all Products.\n" +
-                        "2. List all Books.\n" +
+                        "2. List all Book.\n" +
                         "3. Search a particular book.\n" +
                         "4. Store a new book.\n" +
                         "5. Purchase a book.\n" +
