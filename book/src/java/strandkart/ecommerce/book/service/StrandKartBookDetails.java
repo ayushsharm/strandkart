6da23d861a392921734e225ee5f84ad4f679d6b5
Ayush Sharma <ayush@panhala.strandls.com>
2019-08-27 19:32:59 +0530

    Interface class

--- book/src/java/strandkart/ecommerce/book/service/StrandKartBookDetails.java
+++ book/src/java/strandkart/ecommerce/book/service/StrandKartBookDetails.java
@@ -1,6 +1,8 @@
 package strandkart.ecommerce.book.service;
 
+import strandkart.ecommerce.book.Binding;
 import strandkart.ecommerce.book.Datamodel.Book;
+import strandkart.ecommerce.product.productstype.ProductType;
 
 import java.util.List;
 
@@ -8,7 +10,9 @@ public interface StrandKartBookDetails {
 
     List<Book> getAllBooks();
 
-    List<Book> searchBookUsingTitle(String bookName);
-    }
+    Book searchBookUsingTitle(String bookName);
 
+    Book searchBookUsingIsbn(List<Book> books, String isbn);
+
+    void addNewBook(ProductType productType, String title, String author, String isbn, String publisher, String language, String year, Binding binding, Double price);
 }
