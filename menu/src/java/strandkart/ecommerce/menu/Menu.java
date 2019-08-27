3206c5e8456907d0a5b235ff7155befad5ccb21b
Ayush Sharma <ayush@panhala.strandls.com>
2019-08-27 19:49:53 +0530

    Modifications in the menu

--- menu/src/java/strandkart/ecommerce/menu/Menu.java
+++ menu/src/java/strandkart/ecommerce/menu/Menu.java
@@ -1,25 +1,20 @@
 package strandkart.ecommerce.menu;
 
 import com.google.inject.Inject;
-import org.json.simple.JSONObject;
 import strandkart.ecommerce.book.Binding;
 import strandkart.ecommerce.book.Datamodel.Book;
+import strandkart.ecommerce.book.impl.StrandkartBookDetailsImpl;
 import strandkart.ecommerce.book.service.StrandKartBookDetails;
-import strandkart.ecommerce.orders.DataModels.Order;
 import strandkart.ecommerce.product.productstype.ProductType;
-import strandkart.ecommerce.user.datamodel.User;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
-    @Inject
-    static
-    StrandKartBookDetails strandKartBookDetails;
-
 
     public static void main(String[] args) throws IOException {
+        StrandKartBookDetails strandKartBookDetails = new StrandkartBookDetailsImpl();
 
         System.out.println("WELCOME TO STRAND-KART.\n" +
                 "Select Option to Continue\n" +
@@ -56,21 +51,26 @@ public class Menu {
                     input.nextLine();
                     System.out.println("Please enter the name of the book");
                     String bookName = input.nextLine();
-                    strandKartBookDetails
-                    if(booksFound.size()==0){
-                        System.out.println("No book with this title is available on StrandKart.");
+                    Book book = strandKartBookDetails.searchBookUsingTitle(bookName);
+                    if(book==null){
+                        System.out.println("No books available with this title.");
+                        break;
                     }
-                    else if(booksFound.size()>1){
-                        System.out.println("Multiple books found with the same title. Please specify the ISBN number");
-                        String isbn = input.nextLine();
-                        HashMap<String, Object> bookFound = findMatchingBookWithISBN(booksFound, isbn);
-                        if(bookFound==null){
-                            System.out.println("No book with given ISBN number and Title is available on StrandKart.");
-                        }
-                        else{
-                            System.out.println(bookFound);
-                        }
-                    }
+                    System.out.println(book.toString());
+//                    List<Book> books = strandKartBookDetails.searchBookUsingTitle(bookName);
+//                    if(books.size()==0){
+//                        System.out.println("No books available with this title.");
+//                        break;
+//                    }
+//                    if(books.size()==1) {
+//                        System.out.println(books.get(0).toString());
+//                    }
+//                    else{
+//                        System.out.println("Multiple books found with same title. Please provide ISBN number : ");
+//                        String isbn = input.nextLine();
+//                        Book book = strandKartBookDetails.searchBookUsingIsbn(books, isbn);
+//                        System.out.println(book.toString());
+//                    }
                     break;
                 case 4:
                     input.nextLine();
@@ -97,76 +97,32 @@ public class Menu {
                         binding = Binding.DIGITAL;
                     }
                     System.out.print("Price : ");
-                    Float price = input.nextFloat();
-                    Book book = new Book(ProductType.BOOKS, title, author, isbn, publisher, language, year, binding, price);
-                    allBooks.add(book);
-                    bookMap.put("Title", title);
-                    bookMap.put("Author", author);
-                    bookMap.put("ISBN", isbn);
-                    bookMap.put("Publisher", publisher);
-                    bookMap.put("Language", language);
-                    bookMap.put("Year", year);
-                    bookMap.put("Binding", binding);
-                    bookMap.put("Price", price);
-                    bookMapList.add(bookMap);
+                    Double price = input.nextDouble();
+                    ProductType productType = ProductType.BOOKS;
+                    strandKartBookDetails.addNewBook(productType, title, author, isbn, publisher, language, year, binding, price);
                     break;
                 case 5:
+                    input.nextLine();
                     System.out.println("Welcome to StrandKart Order placement.\n Please enter the book Title you want to purchase");
-                    String title1 = input.nextLine();
-                    Set<HashMap<String, Object>> booksfound = findMatchingBooksWithTitle(bookMapList, title1);
-                    HashMap<String, Object> bookfound = null;
-                    if(booksfound.size()==0){
+                    title = input.nextLine();
+                    book = strandKartBookDetails.searchBookUsingTitle(title);
+                    if(book==null){
                         System.out.println("No book with this title is available on StrandKart.");
                         break;
                     }
-                    else if(booksfound.size()>1) {
-                        System.out.println("Multiple books found with the same title. Please specify the ISBN number");
-                        String Isbn = input.nextLine();
-                        bookfound = findMatchingBookWithISBN(booksfound, Isbn);
-                        if(bookfound==null){
-                            System.out.println("No book with given ISBN number and Title is available on StrandKart.");
-                            break;
-                        }
-                    }
-                    System.out.println("Please Enter your Name : ");
-                    String userName = input.nextLine();
-                    System.out.println("Please Enter your contact Number : ");
-                    String contactNumber = input.nextLine();
-                    System.out.println("Please Enter the quantity of items you want to purchase.");
-                    Integer quantity = input.nextInt();
-                    if(quantity>0){
-                        Binding binding1 = null;
-                        if(bookfound.get("Binding").toString().equals("PAPERBACK")){
-                            binding1 = Binding.PAPERBACK;
-                        }
-                        else if(bookfound.get("Binding").toString().equals("HARDBOUND")) {
-                            binding1 = Binding.HARDBOUND;
-                        }
-                        else if(bookfound.get("Binding").toString().equals("DIGITAL")){
-                            binding1 = Binding.DIGITAL;
-                        }
-                        Book bookToPurchase = new Book(ProductType.BOOKS, bookfound.get("Title").toString(),
-                                bookfound.get("Author").toString(),bookfound.get("ISBN").toString(),
-                                bookfound.get("Publisher").toString(), bookfound.get("Language").toString(),
-                                bookfound.get("Year").toString(), binding1, (Float)bookfound.get("Price"));
-                        User user = new User(userName, contactNumber);
-                        Float orderAmount = quantity*((Float)bookfound.get("Price"));
-                        Order order = new Order(bookToPurchase, user, quantity, Calendar.getInstance().getTime(), orderAmount);
-                        orderList.add(order);
-                    }
+                    System.out.println("How many books would you like to purchase");
+                    int quantity = input.nextInt();
+                    Double orderAmount = quantity*book.getPrice();
+                    System.out.println("Order placed for book : " + book.toString()+"Total amount : ₹" + orderAmount+"\n");
+//                    System.out.println("Please Enter your Name : ");
+//                    String userName = input.nextLine();
+//                    System.out.println("Please Enter your contact Number : ");
+//                    String contactNumber = input.nextLine();
+//                    System.out.println("Please Enter the quantity of items you want to purchase.");
+//                    Integer quantity = input.nextInt();
                     break;
                 case 6:
                     System.out.println("Thank you for using StrandKart");
-                    for (HashMap<String, Object> bookmap : bookMapList) {
-                        JSONObject bookJSON = new JSONObject(bookmap);
-                        bookJSONList.add(bookJSON);
-                    }
-                    System.out.println(bookJSONList.size());
-                    FileWriter fileWriter = new FileWriter("books.txt");
-                    for (int i = 0; i < bookJSONList.size(); i++) {
-                        fileWriter.write(bookJSONList.get(i).toJSONString());
-                    }
-                    fileWriter.close();
                     break;
             }
             if (choice != 6) {
