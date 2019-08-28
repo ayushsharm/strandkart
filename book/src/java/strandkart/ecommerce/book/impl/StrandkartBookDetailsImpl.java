c2314720b629aded99aa688f7d02fec2c93b6ab3
Ayush Sharma <ayush@panhala.strandls.com>
2019-08-28 17:02:17 +0530

    Added init and close methods

--- book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
+++ book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
@@ -2,70 +2,84 @@ package strandkart.ecommerce.book.impl;
 
 import strandkart.ecommerce.book.Binding;
 import strandkart.ecommerce.book.Datamodel.Book;
+import strandkart.ecommerce.book.FileHandler.BookFileReaderWriter;
 import strandkart.ecommerce.book.service.StrandKartBookDetails;
 import strandkart.ecommerce.product.productstype.ProductType;
 
+import java.io.IOException;
 import java.util.*;
 
 public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
 
-    public StrandkartBookDetailsImpl(){
-        init();
+
+    public StrandkartBookDetailsImpl(String fileName) throws IOException {
+        init(fileName);
     }
 
-    private static List<Book> allBooks = new ArrayList<Book>(Arrays.asList(
-            new Book(ProductType.BOOKS, "Harry Potter 1", "J. K. Rowling", "isbn123", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 3", "J. K. Rowling", "isbn234", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 2", "J. K. Rowling", "isbn345", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 4", "J. K. Rowling", "isbn456", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 5", "J. K. Rowling", "isbn567", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 6", "J. K. Rowling", "isbn458", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 7", "J. K. Rowling", "isbn754", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 8", "J. K. Rowling", "isbn842", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 9", "J. K. Rowling", "isbn451", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 10", "J. K. Rowling", "isbn412", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 11", "J. K. Rowling", "isbn542", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 12", "J. K. Rowling", "isbn874", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 13", "J. K. Rowling", "isbn321", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 14", "J. K. Rowling", "isbn654", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 15", "J. K. Rowling", "isbn777", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 16", "J. K. Rowling", "isbn888", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 17", "J. K. Rowling", "isbn555", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 18", "J. K. Rowling", "isbn000", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 19", "J. K. Rowling", "isbn007", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 20", "J. K. Rowling", "isbn008", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 21", "J. K. Rowling", "isbn413", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-            new Book(ProductType.BOOKS, "Harry Potter 22", "J. K. Rowling", "isbn333", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66)
-    ));
-
-    private static TreeMap<String, Book> isbnTitleMap = new TreeMap<String, Book>(){{
-        put("Harry Potter 1", allBooks.get(0));
-        put("Harry Potter 2", allBooks.get(1));
-        put("Harry Potter 3", allBooks.get(2));
-        put("Harry Potter 4", allBooks.get(3));
-        put("Harry Potter 5", allBooks.get(4));
-        put("Harry Potter 6", allBooks.get(5));
-        put("Harry Potter 7", allBooks.get(6));
-        put("Harry Potter 8", allBooks.get(7));
-        put("Harry Potter 9", allBooks.get(8));
-        put("Harry Potter 10", allBooks.get(9));
-        put("Harry Potter 11", allBooks.get(10));
-        put("Harry Potter 12", allBooks.get(11));
-        put("Harry Potter 13", allBooks.get(12));
-        put("Harry Potter 14", allBooks.get(13));
-        put("Harry Potter 15", allBooks.get(14));
-        put("Harry Potter 16", allBooks.get(15));
-        put("Harry Potter 17", allBooks.get(16));
-        put("Harry Potter 18", allBooks.get(17));
-        put("Harry Potter 19", allBooks.get(18));
-        put("Harry Potter 20", allBooks.get(19));
-        put("Harry Potter 21", allBooks.get(20));
-        put("Harry Potter 22", allBooks.get(21));
-    }};
 
 
-    private void init() {
-        
+    private static List<Book> allBooks = new ArrayList<Book>();
+//    private static List<Book> allBooks = new ArrayList<Book>(Arrays.asList(
+//            new Book(ProductType.BOOKS, "Harry Potter 1", "J. K. Rowling", "isbn123", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 3", "J. K. Rowling", "isbn234", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 2", "J. K. Rowling", "isbn345", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 4", "J. K. Rowling", "isbn456", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 5", "J. K. Rowling", "isbn567", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 6", "J. K. Rowling", "isbn458", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 7", "J. K. Rowling", "isbn754", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 8", "J. K. Rowling", "isbn842", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 9", "J. K. Rowling", "isbn451", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 10", "J. K. Rowling", "isbn412", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 11", "J. K. Rowling", "isbn542", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 12", "J. K. Rowling", "isbn874", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 13", "J. K. Rowling", "isbn321", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 14", "J. K. Rowling", "isbn654", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 15", "J. K. Rowling", "isbn777", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 16", "J. K. Rowling", "isbn888", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 17", "J. K. Rowling", "isbn555", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 18", "J. K. Rowling", "isbn000", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 19", "J. K. Rowling", "isbn007", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 20", "J. K. Rowling", "isbn008", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 21", "J. K. Rowling", "isbn413", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
+//            new Book(ProductType.BOOKS, "Harry Potter 22", "J. K. Rowling", "isbn333", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66)
+//    ));
+
+    private static TreeMap<String, Book> titleBookMap = new TreeMap<String, Book>();
+    private static TreeMap<Integer, Book > yearBookMap = new TreeMap<Integer, Book>();
+//    private static TreeMap<String, Book> isbnTitleMap = new TreeMap<String, Book>(){{
+//        put("Harry Potter 1", allBooks.get(0));
+//        put("Harry Potter 2", allBooks.get(1));
+//        put("Harry Potter 3", allBooks.get(2));
+//        put("Harry Potter 4", allBooks.get(3));
+//        put("Harry Potter 5", allBooks.get(4));
+//        put("Harry Potter 6", allBooks.get(5));
+//        put("Harry Potter 7", allBooks.get(6));
+//        put("Harry Potter 8", allBooks.get(7));
+//        put("Harry Potter 9", allBooks.get(8));
+//        put("Harry Potter 10", allBooks.get(9));
+//        put("Harry Potter 11", allBooks.get(10));
+//        put("Harry Potter 12", allBooks.get(11));
+//        put("Harry Potter 13", allBooks.get(12));
+//        put("Harry Potter 14", allBooks.get(13));
+//        put("Harry Potter 15", allBooks.get(14));
+//        put("Harry Potter 16", allBooks.get(15));
+//        put("Harry Potter 17", allBooks.get(16));
+//        put("Harry Potter 18", allBooks.get(17));
+//        put("Harry Potter 19", allBooks.get(18));
+//        put("Harry Potter 20", allBooks.get(19));
+//        put("Harry Potter 21", allBooks.get(20));
+//        put("Harry Potter 22", allBooks.get(21));
+//    }};
+
+
+    private void init(String fileName) throws IOException {
+        BookFileReaderWriter bookFileReaderWriter = new BookFileReaderWriter(fileName);
+        List<Book> books = bookFileReaderWriter.readFromFile();
+        allBooks = books;
+        for(Book book: books){
+            titleBookMap.put(book.getTitle(), book);
+        }
+
     }
 
     public List<Book> getAllBooks() {
@@ -74,8 +88,8 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
     }
 
     public Book searchBookUsingTitle(String bookName) {
-        if(isbnTitleMap.containsKey(bookName)){
-            return isbnTitleMap.get(bookName);
+        if(titleBookMap.containsKey(bookName)){
+            return titleBookMap.get(bookName);
         }
         return null;
     }
@@ -90,11 +104,16 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
     }
 
     public void addNewBook(ProductType productType, String title, String author, String isbn, String publisher, String language, String year, Binding binding, Double price){
-        if(isbnTitleMap.containsKey(title)){
+        if(titleBookMap.containsKey(title)){
             System.out.println("Book already present");
             return;
         }
         allBooks.add(new Book(productType, title, author, isbn, publisher, language, year, binding, price));
+    }
+
+    public void close(String fileName) throws IOException {
+        BookFileReaderWriter bookFileReaderWriter = new BookFileReaderWriter(fileName);
+        bookFileReaderWriter.writeToFile(allBooks);
     }
 
 }
