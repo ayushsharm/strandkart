8f1ce4c1c70462c6a3c33a2cf7fdf41334d5a273
Ayush Sharma <ayush@panhala.strandls.com>
2019-08-28 17:02:59 +0530

    Added method name init

--- book/src/java/strandkart/ecommerce/book/service/StrandKartBookDetails.java
+++ book/src/java/strandkart/ecommerce/book/service/StrandKartBookDetails.java
@@ -4,6 +4,7 @@ import strandkart.ecommerce.book.Binding;
 import strandkart.ecommerce.book.Datamodel.Book;
 import strandkart.ecommerce.product.productstype.ProductType;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface StrandKartBookDetails {
@@ -15,4 +16,8 @@ public interface StrandKartBookDetails {
     Book searchBookUsingIsbn(List<Book> books, String isbn);
 
     void addNewBook(ProductType productType, String title, String author, String isbn, String publisher, String language, String year, Binding binding, Double price);
+
+    void close(String filename) throws IOException;
+
+
 }
