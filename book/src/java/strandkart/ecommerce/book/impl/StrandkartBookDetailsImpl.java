6dc3d7990ee1f0afde907f1f7386b75a5c02a8e2
Ayush Sharma <ayush.sharma@strandls.com>
2019-09-03 10:58:39 +0530

    aa

--- book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
+++ book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
@@ -3,6 +3,8 @@ package strandkart.ecommerce.book.impl;
 import strandkart.ecommerce.book.Binding;
 import strandkart.ecommerce.book.Datamodel.Book;
 import strandkart.ecommerce.book.FileHandler.BookFileReaderWriter;
+import strandkart.ecommerce.book.Sorting;
+import strandkart.ecommerce.book.SortingOrder;
 import strandkart.ecommerce.book.service.StrandKartBookDetails;
 import strandkart.ecommerce.product.productstype.ProductType;
 
@@ -12,74 +14,23 @@ import java.util.*;
 public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
 
 
+    private static List<Book> allBooks = new ArrayList<Book>();
+    private static TreeMap<String, List<Book>> titleBookMap = new TreeMap<String, List<Book>>();
+    private static TreeMap<String, List<Book>> yearBookMap = new TreeMap<String, List<Book>>();
+    private static TreeMap<String, List<Book>> authorBookMap = new TreeMap<String, List<Book>>();
+
     public StrandkartBookDetailsImpl(String fileName) throws IOException {
         init(fileName);
     }
 
-
-
-    private static List<Book> allBooks = new ArrayList<Book>();
-//    private static List<Book> allBooks = new ArrayList<Book>(Arrays.asList(
-//            new Book(ProductType.BOOKS, "Harry Potter 1", "J. K. Rowling", "isbn123", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 3", "J. K. Rowling", "isbn234", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 2", "J. K. Rowling", "isbn345", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 4", "J. K. Rowling", "isbn456", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 5", "J. K. Rowling", "isbn567", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 6", "J. K. Rowling", "isbn458", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 7", "J. K. Rowling", "isbn754", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 8", "J. K. Rowling", "isbn842", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 9", "J. K. Rowling", "isbn451", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 10", "J. K. Rowling", "isbn412", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 11", "J. K. Rowling", "isbn542", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 12", "J. K. Rowling", "isbn874", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 13", "J. K. Rowling", "isbn321", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 14", "J. K. Rowling", "isbn654", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 15", "J. K. Rowling", "isbn777", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 16", "J. K. Rowling", "isbn888", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 17", "J. K. Rowling", "isbn555", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 18", "J. K. Rowling", "isbn000", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 19", "J. K. Rowling", "isbn007", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 20", "J. K. Rowling", "isbn008", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 21", "J. K. Rowling", "isbn413", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66),
-//            new Book(ProductType.BOOKS, "Harry Potter 22", "J. K. Rowling", "isbn333", "Scholastic", "English ", "1997", Binding.HARDBOUND, 232.66)
-//    ));
-
-    private static TreeMap<String, Book> titleBookMap = new TreeMap<String, Book>();
-    private static TreeMap<Integer, Book > yearBookMap = new TreeMap<Integer, Book>();
-//    private static TreeMap<String, Book> isbnTitleMap = new TreeMap<String, Book>(){{
-//        put("Harry Potter 1", allBooks.get(0));
-//        put("Harry Potter 2", allBooks.get(1));
-//        put("Harry Potter 3", allBooks.get(2));
-//        put("Harry Potter 4", allBooks.get(3));
-//        put("Harry Potter 5", allBooks.get(4));
-//        put("Harry Potter 6", allBooks.get(5));
-//        put("Harry Potter 7", allBooks.get(6));
-//        put("Harry Potter 8", allBooks.get(7));
-//        put("Harry Potter 9", allBooks.get(8));
-//        put("Harry Potter 10", allBooks.get(9));
-//        put("Harry Potter 11", allBooks.get(10));
-//        put("Harry Potter 12", allBooks.get(11));
-//        put("Harry Potter 13", allBooks.get(12));
-//        put("Harry Potter 14", allBooks.get(13));
-//        put("Harry Potter 15", allBooks.get(14));
-//        put("Harry Potter 16", allBooks.get(15));
-//        put("Harry Potter 17", allBooks.get(16));
-//        put("Harry Potter 18", allBooks.get(17));
-//        put("Harry Potter 19", allBooks.get(18));
-//        put("Harry Potter 20", allBooks.get(19));
-//        put("Harry Potter 21", allBooks.get(20));
-//        put("Harry Potter 22", allBooks.get(21));
-//    }};
-
-
     private void init(String fileName) throws IOException {
-        BookFileReaderWriter bookFileReaderWriter = new BookFileReaderWriter(fileName);
-        List<Book> books = bookFileReaderWriter.readFromFile();
-        allBooks = books;
-        for(Book book: books){
-            titleBookMap.put(book.getTitle(), book);
-        }
 
+        BookFileReaderWriter bookFileReaderWriter = new BookFileReaderWriter(fileName);
+        allBooks = bookFileReaderWriter.readFromFile();
+
+        for (Book book : allBooks) {
+            addToMaps(book);
+        }
     }
 
     public List<Book> getAllBooks() {
@@ -87,28 +38,75 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
         return allBooks;
     }
 
-    public Book searchBookUsingTitle(String bookName) {
-        if(titleBookMap.containsKey(bookName)){
+    public List<Book> searchBookUsingTitle(String bookName) {
+        if (titleBookMap.containsKey(bookName)) {
             return titleBookMap.get(bookName);
         }
         return null;
     }
 
     public Book searchBookUsingIsbn(List<Book> books, String isbn) {
-        for(Book book : books){
-            if(book.getISBN().equals(isbn)){
+        for (Book book : books) {
+            if (book.getISBN().equals(isbn)) {
                 return book;
             }
         }
         return null;
     }
 
-    public void addNewBook(ProductType productType, String title, String author, String isbn, String publisher, String language, String year, Binding binding, Double price){
-        if(titleBookMap.containsKey(title)){
-            System.out.println("Book already present");
-            return;
+    public void addNewBook(Book book) {
+        allBooks.add(book);
+        addToMaps(book);
+    }
+
+    private void addToMaps(Book book) {
+        List<Book> books1 = Collections.singletonList(book);
+        List<Book> tempBookList = new ArrayList<Book>();
+        if (titleBookMap.containsKey(book.getTitle())) {
+            List<Book> bookfromMap = titleBookMap.get(book.getTitle());
+            tempBookList.addAll(bookfromMap);
+            tempBookList.add(book);
+            titleBookMap.put(book.getTitle(), tempBookList);
+            tempBookList.clear();
         }
-        allBooks.add(new Book(productType, title, author, isbn, publisher, language, year, binding, price));
+        else{
+            titleBookMap.put(book.getTitle(),books1);
+        }
+
+        if(authorBookMap.containsKey(book.getAuthor())){
+            List<Book> bookfromMap = authorBookMap.get(book.getAuthor());
+            tempBookList.addAll(bookfromMap);
+            tempBookList.add(book);
+            titleBookMap.put(book.getAuthor(), tempBookList);
+            tempBookList.clear();
+        }
+        else{
+            authorBookMap.put(book.getAuthor(),books1);
+        }
+
+        if(yearBookMap.containsKey(book.getYear())){
+            List<Book> bookfromMap = yearBookMap.get(book.getYear());
+            tempBookList.addAll(bookfromMap);
+            tempBookList.add(book);
+            titleBookMap.put(book.getTitle(), tempBookList);
+            tempBookList.clear();
+        }
+        else{
+            authorBookMap.put(book.getYear(),books1);
+        }
+    }
+
+    public List<Book> getSortedBookList(Sorting sorting, SortingOrder sortingOrder) {
+        List<Book> booksSorted = new ArrayList<Book>();
+        if (sorting == Sorting.AUTHOR) {
+            booksSorted = createListWithTreeMap(authorBookMap);
+        } else if (sorting == Sorting.TITLE) {
+            booksSorted = createListWithTreeMap(titleBookMap);
+        }
+        else{
+            booksSorted = createListWithTreeMap(yearBookMap);
+        }
+        return booksSorted;
     }
 
     public void close(String fileName) throws IOException {
@@ -116,4 +114,16 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
         bookFileReaderWriter.writeToFile(allBooks);
     }
 
+    private List<Book> createListWithTreeMap(TreeMap<String, List<Book>> map) {
+        Set set = map.entrySet();
+        Iterator iterator = set.iterator();
+        List<Book> booksSorted = new ArrayList<Book>();
+
+        while (iterator.hasNext()) {
+            Map.Entry me = (Map.Entry) iterator.next();
+            List<Book> tempBookList = map.get(me.getKey());
+            booksSorted.addAll(tempBookList);
+        }
+        return booksSorted;
+    }
 }
