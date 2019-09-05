ce097d7614941f28c21674802e6fe93ef1727407
Ayush Sharma <ayush@panhala.strandls.com>
2019-09-05 19:05:49 +0530

    aa

--- book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
+++ book/src/java/strandkart/ecommerce/book/impl/StrandkartBookDetailsImpl.java
@@ -1,5 +1,6 @@
 package strandkart.ecommerce.book.impl;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import strandkart.ecommerce.book.Datamodel.Book;
 import strandkart.ecommerce.book.FileHandler.BookFileReaderWriter;
 import strandkart.ecommerce.book.Sorting;
@@ -23,13 +24,16 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
 
     private void init(String fileName) throws IOException {
 
+        long readStartTIme = System.currentTimeMillis();
         BookFileReaderWriter bookFileReaderWriter = new BookFileReaderWriter(fileName);
         allBooks = bookFileReaderWriter.readFromFile();
+        long readEndTIme = System.currentTimeMillis();
+        System.out.println(String.format("Read time = %d", readEndTIme - readStartTIme));
 
-
-        for (Book book : allBooks) {
-            addToMaps(book);
-        }
+        long mapInitStartTime = System.currentTimeMillis();
+        addToMaps(allBooks);
+        long mapInitEndTime = System.currentTimeMillis();
+        System.out.println(String.format("Map init time = %d", mapInitEndTime - mapInitStartTime));
     }
 
     public List<Book> getAllBooks() {
@@ -55,76 +59,73 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
 
     public void addNewBook(Book book) {
         allBooks.add(book);
-        addToMaps(book);
+        addToMaps(Collections.singletonList(book));
     }
 
-    private void addToMaps(Book book) {
+    private void addToMaps(List<Book> books) {
         List<Book> books1 = new ArrayList<Book>();
-        books1.add(book);
-        List<Book> tempBookList = new ArrayList<Book>();
-        if (titleBookMap.containsKey(book.getTitle())) {
-            List<Book> bookfromMap = titleBookMap.get(book.getTitle());
-//            if(bookfromMap.size()==0){
-//                System.out.println("Why zero");
-//            }
-            tempBookList.addAll(bookfromMap);
-            tempBookList.add(book);
-            titleBookMap.put(book.getTitle(), tempBookList);
-            tempBookList.clear();
-        } else {
-            titleBookMap.put(book.getTitle(), books1);
-        }
+        for (Book book : books) {
+            books1.add(book);
+            List<Book> tempBookList = new ArrayList<Book>();
+            if (titleBookMap.containsKey(book.getTitle())) {
+                List<Book> bookfromMap = titleBookMap.get(book.getTitle());
+                tempBookList.addAll(bookfromMap);
+                tempBookList.add(book);
+                titleBookMap.put(book.getTitle(), tempBookList);
+                tempBookList.clear();
+            } else {
+                titleBookMap.put(book.getTitle(), books1);
+            }
 
-        if (authorBookMap.containsKey(book.getAuthor())) {
-            List<Book> bookfromMap = authorBookMap.get(book.getAuthor());
-            tempBookList.addAll(bookfromMap);
-            tempBookList.add(book);
-            authorBookMap.put(book.getAuthor(), tempBookList);
-            tempBookList.clear();
-        } else {
-            authorBookMap.put(book.getAuthor(), books1);
-        }
+            if (authorBookMap.containsKey(book.getAuthor())) {
+                List<Book> bookfromMap = authorBookMap.get(book.getAuthor());
+                tempBookList.addAll(bookfromMap);
+                tempBookList.add(book);
+                authorBookMap.put(book.getAuthor(), tempBookList);
+                tempBookList.clear();
+            } else {
+                authorBookMap.put(book.getAuthor(), books1);
+            }
 
-        if (yearBookMap.containsKey(book.getYear())) {
-            List<Book> bookfromMap = yearBookMap.get(book.getYear());
-            tempBookList.addAll(bookfromMap);
-            tempBookList.add(book);
-            yearBookMap.put(book.getYear(), tempBookList);
-            tempBookList.clear();
-        } else {
-            yearBookMap.put(book.getYear(), books1);
+            if (yearBookMap.containsKey(book.getYear())) {
+                List<Book> bookfromMap = yearBookMap.get(book.getYear());
+                tempBookList.addAll(bookfromMap);
+                tempBookList.add(book);
+                yearBookMap.put(book.getYear(), tempBookList);
+                tempBookList.clear();
+            } else {
+                yearBookMap.put(book.getYear(), books1);
+            }
+            books1.clear();
         }
     }
 
     public TreeMap<String, List<Book>> getSortedBookList(Sorting sorting, SortingOrder sortingOrder) {
 
         if (sorting == Sorting.AUTHOR) {
-            if(sortingOrder==SortingOrder.ASCENDING){
+            if (sortingOrder == SortingOrder.ASCENDING) {
                 return authorBookMap;
-            }
-            else{
+            } else {
                 return getReverseMap(authorBookMap);
             }
         } else if (sorting == Sorting.TITLE) {
-            if(sortingOrder == SortingOrder.ASCENDING){
+            if (sortingOrder == SortingOrder.ASCENDING) {
                 return titleBookMap;
-            }
-            else {
+            } else {
                 return getReverseMap(titleBookMap);
             }
         } else {
-            if(sortingOrder == SortingOrder.ASCENDING){
+            if (sortingOrder == SortingOrder.ASCENDING) {
                 return yearBookMap;
-            }
-            else{
+            } else {
                 return getReverseMap(yearBookMap);
             }
         }
     }
 
-    public TreeMap<String, List<Book>> getReverseMap(TreeMap<String, List<Book>> map){
+    public TreeMap<String, List<Book>> getReverseMap(TreeMap<String, List<Book>> map) {
         TreeMap<String, List<Book>> reverseMap = new TreeMap<String, List<Book>>(Collections.<String>reverseOrder());
-        for (Map.Entry<String, List<Book>> entry : map.entrySet()){
+        for (Map.Entry<String, List<Book>> entry : map.entrySet()) {
             reverseMap.put(entry.getKey(), entry.getValue());
         }
         return reverseMap;
@@ -139,18 +140,18 @@ public class StrandkartBookDetailsImpl implements StrandKartBookDetails {
         Set set = map.entrySet();
         Iterator iterator = set.iterator();
         List<Book> booksSorted = new ArrayList<Book>();
-        int counter=0;
+        int counter = 0;
         while (iterator.hasNext()) {
             Map.Entry me = (Map.Entry) iterator.next();
             List<Book> tempBookList = map.get(me.getKey());
-            if(tempBookList.size()>1){
+            if (tempBookList.size() > 1) {
                 counter++;
             }
-            for(Book book: tempBookList){
+            for (Book book : tempBookList) {
                 booksSorted.add(book);
             }
         }
-        System.out.println("Counter for 0 = "+counter);
+        System.out.println("Counter for 0 = " + counter);
         System.out.println(booksSorted.size());
         return booksSorted;
     }
