34487f3ba75f6b9c89111055e87e9472db5ab2b2
Ayush Sharma <ayush@panhala.strandls.com>
2019-09-09 20:07:55 +0530

    Changes as per Service

--- orders/src/java/strandkart/ecommerce/orders/impl/OrderManagementImpl.java
+++ orders/src/java/strandkart/ecommerce/orders/impl/OrderManagementImpl.java
@@ -4,6 +4,7 @@ import strandkart.ecommerce.book.Datamodel.Book;
 import strandkart.ecommerce.orders.DataModels.Order;
 import strandkart.ecommerce.orders.service.OrderManagement;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -12,10 +13,14 @@ import java.util.List;
 public class OrderManagementImpl implements OrderManagement {
 
 
-    private List<Order> orders = Collections.emptyList();
+    private List<Order> orders = new ArrayList<Order>();
 
-    public void orderBook(Book book, int quantity, Date purchaseDate, Float orderAmount) {
-        Order order = new Order(book, quantity, purchaseDate, orderAmount);
+    public void orderBook(Book book, int quantity, Double orderAmount) {
+        Order order = new Order(book, quantity, orderAmount);
+        orders.add(order);
+    }
+
+    public void orderBook(Order order){
         orders.add(order);
     }
 
