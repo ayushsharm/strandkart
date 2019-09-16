0992fbb252bf56190486e95c265660bdd3796c1e
Ayush Sharma <ayush@panhala.strandls.com>
2019-09-16 19:17:44 +0530

    Datamodel -> datamodel

---
+++ orders/src/java/strandkart/ecommerce/orders/datamodels/Order.java
@@ -0,0 +1,52 @@
+package strandkart.ecommerce.orders.datamodels;
+
+import strandkart.ecommerce.book.datamodel.Book;
+//import strandkart.ecommerce.user.datamodel.User;
+
+
+public class Order {
+
+    private Book book;
+
+//    private User user;
+
+    private Integer quantity;
+
+//    private Date purchaseDate;
+
+    private Double orderAmount;
+
+    public Order(Book book, int quantity, Double orderAmount){
+        this.book = book;
+//        this.user = user;
+        this.quantity = quantity;
+//        this.purchaseDate = purchaseDate;
+        this.orderAmount = orderAmount;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+//    public User getUser() {
+//        return user;
+//    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+//    public Date getPurchaseDate() {
+//        return purchaseDate;
+//    }
+
+    public Double getOrderAmount() {
+        return orderAmount;
+    }
+
+    @Override
+    public String toString(){
+        return book.toString()+"\nQuantity : "+quantity+"\nOrder Amount : ₹"+orderAmount;
+    }
+
+}
