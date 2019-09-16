1bb85fae1b0d12c379a449cfd771d3b1a9933ae5
Ayush Sharma <ayush@panhala.strandls.com>
2019-09-16 19:16:33 +0530

    Datamodel -> datamodel

---
+++ product/src/java/strandkart/ecommerce/product/datamodel/Product.java
@@ -0,0 +1,12 @@
+package strandkart.ecommerce.product.datamodel;
+
+import strandkart.ecommerce.product.productstype.ProductType;
+
+public class Product {
+    private ProductType type;
+
+    public Product(ProductType type) {
+        this.type = type;
+    }
+
+}
