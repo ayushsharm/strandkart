f6de0ece6ed13bd21461bdf9dbe1242f88e2c7bf
Ayush Sharma <ayush@panhala.strandls.com>
2019-09-25 12:28:32 +0530

    Added method to check if order is already present

--- orders/src/java/strandkart/ecommerce/orders/impl/OrderManagementImpl.java
+++ orders/src/java/strandkart/ecommerce/orders/impl/OrderManagementImpl.java
@@ -13,15 +13,33 @@ public class OrderManagementImpl implements OrderManagement {
 
     private List<Order> orders = new ArrayList<Order>();
 
+    @Override
     public void orderBook(Book book, int quantity, Double orderAmount) {
         Order order = new Order(book, quantity, orderAmount);
+        Order order1 = containsOrder(order);
+        if (order1 != null) {
+            order1.increaseQuantity(order.getQuantity());
+        } else {
+            orders.add(order);
+        }
+    }
+
+    private Order containsOrder(Order order1) {
+        for (Order order : orders) {
+            if (order.getBook().getTitle().equals(order1.getBook().getTitle()) &&
+                    order.getBook().getISBN().equals(order1.getBook().getISBN())){
+                return order;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void orderBook(Order order) {
         orders.add(order);
     }
 
-    public void orderBook(Order order){
-        orders.add(order);
-    }
-
+    @Override
     public List<Order> showAllOrders() {
         return orders;
     }
